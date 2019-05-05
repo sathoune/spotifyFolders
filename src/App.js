de43dfebc59f6560b07401733bc5cde4a4587231
m <michal5bargiel@gmail.com>
2019-05-05 17:25:07 +0200

    Saving albums in state

--- src/App.js
+++ src/App.js
@@ -1,79 +1,49 @@
 import React, {Component} from "react";
-import * as $ from "jquery";
 import hash from "./hash";
-import Player from "./Player";
 import "./App.css";
-import { Login } from "./Login";
-import { ajax } from "./ajax";
+import {Login} from "./Login";
+import {ajax} from "./ajax";
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			token: null,
-			item: {
-				album: {
-					images: [{url: ""}]
-				},
-				name: "",
-				artists: [{name: ""}],
-				duration_ms: 0,
-			},
-			is_playing: "Paused",
-			progress_ms: 0
+			albums: [],
+			albumsLoaded: false,
 		};
-		this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+		this.getAlbums = this.getAlbums.bind(this);
 	}
 	
 	componentDidMount() {
-		// Set token
-		let _token = hash.access_token;
+		const _token = hash.access_token;
 		
 		if (_token) {
-			// Set token
 			this.setState({
-				token: _token
-			});
-			this.getCurrentlyPlaying(_token);
-			this.getAlbums(_token, "https://api.spotify.com/v1/me/albums");
+					token: _token
+				}, () => this.getAlbums("https://api.spotify.com/v1/me/albums")
+			);
 		}
 	}
 	
-	
-	getAlbums(token, url){
+	getAlbums(url) {
 		const options = {
 			url: url,
 			method: "get",
 			headers: {
-				"Authorization": "Bearer " + token
+				"Authorization": "Bearer " + this.state.token
 			},
 			callback: (data) => {
-				console.log(data.items);
-				if (data.next) {
-					this.getAlbums(token, data.next);
-				}
+				this.setState(
+					prev => {
+						return {albums: prev.albums.concat(data.items)}
+					}
+				);
+				if (data.next) this.getAlbums(data.next);
+				else this.setState({albumsLoaded: true});
 			}
 		};
 		ajax(options);
-	}
-	
-	getCurrentlyPlaying(token) {
-		// Make a call using the token
-		$.ajax({
-			url: "https://api.spotify.com/v1/me/player",
-			type: "GET",
-			beforeSend: (xhr) => {
-				xhr.setRequestHeader("Authorization", "Bearer " + token);
-			},
-			success: (data) => {
-			//	console.log("data", data);
-				this.setState({
-					item: data.item,
-					is_playing: data.is_playing,
-					progress_ms: data.progress_ms,
-				});
-			}
-		});
 	}
 	
 	render() {
@@ -82,14 +52,22 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					{(!this.state.token)
-							?
-							( <Login /> )
-							:
-							(<Player
-								item={this.state.item}
-								is_playing={this.state.is_playing}
-								progress_ms={this.progress_ms}
-							/>)
+						?
+						(<Login/>)
+						:
+						(<div>Hello General Kenobi
+							{(!this.state.albumsLoaded)
+								?
+								this.state.albums.length
+								:
+								(this.state.albums.map(
+									album => {
+										console.log(album);
+										return album.album.name
+									})
+								)
+							}
+						</div>)
 					}
 				</header>
 			</div>
